56987bfd6b2a7d817906bd725bd21e7b6c05a22d
Sangam Srivastav <133479736+SangamSrivastav@users.noreply.github.com>
2024-07-01 17:36:42 +0530

    Update myToken.sol

--- myToken.sol
+++ myToken.sol
@@ -6,13 +6,16 @@ contract MyToken {
     string public tokenAbbrv = "ETH";
     uint256 public totalSupply = 0;
 
+    // variable mapping 
     mapping(address => uint) public balances;
 
+    // mint function
     function mint(address _add, uint _value) public {
         totalSupply += _value;
         balances[_add] += _value;
     }
-
+    
+    // burn function
     function burn(address _add, uint _value) public {
         if(balances[_add] >= _value){
         totalSupply -= _value;
